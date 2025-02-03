6731d4446da17894401386d70c5175f11789351a
udvardiendre <udvardiendre@gmail.com>
2025-02-03 21:44:04 +0100

    form 2

--- components/apply-now/apply-now-template.tsx
+++ components/apply-now/apply-now-template.tsx
@@ -15,47 +15,37 @@ type ApplyNowTemplateType = {
   label?: string;
   fieldPlaceholder?: string;
   optional?: boolean;
-}
+};
 
-
-type ApplyNowTemplateProps = ApplyNowTemplateType & {updateFields: (fields: Partial<ApplicationFormSchema>) => void}
+type ApplyNowTemplateProps = ApplyNowTemplateType & {
+  updateFields: (fields: Partial<ApplicationFormSchema>) => void;
+};
 
 const ApplyNowTemplate = ({
-  form,
   fieldName,
   title,
   label,
   fieldPlaceholder,
   optional = false,
-  updateFields
+  updateFields,
 }: ApplyNowTemplateProps) => {
   return (
     <section className="w-full h-[70vh] bg-primary-black">
       <div className="container mx-auto flex flex-col gap-16 px-6 pt-[146px]">
-        <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between">
           <h1 className="text-white text-h3-mobile md:text-h3">{title}</h1>
         </div>
         {fieldName === "isVip" ? (
           <div className="flex flex-col gap-8 md:gap-10 items-start">
             <div className="flex flex-col items-start gap-4 md:gap-[33px]">
-              <FormField
-                control={form.control}
-                name={fieldName}
-                render={({ field }) => (
-                  <FormItem className="text-white flex flex-row-reverse gap-6">
-                    <FormLabel className="!text-form-text">{label}</FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        value={field.value}
-                        type="checkbox"
-                        onChange={(e) => updateFields({ [fieldName]: e.target.checked })}
-                        className="w-[25px] h-[25px] accent-[#3D3F43] rounded-[4px] shadow-inner bg-[#3D3F43]"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
+              <label className="!text-form-text">{label}</label>
+              <input
+                value={value}
+                type="checkbox"
+                onChange={(e) =>
+                  updateFields({ [fieldName]: e.target.checked })
+                }
+                className="w-[25px] h-[25px] accent-[#3D3F43] rounded-[4px] shadow-inner bg-[#3D3F43]"
               />
               <p className="text-white text-form-text">
                 If you're not at $10,000 leave this blank, we will get you there
@@ -63,26 +53,17 @@ const ApplyNowTemplate = ({
               </p>
             </div>
           </div>
-        ): (
-          <FormField
-                control={form.control}
-                name={fieldName}
-                render={({ field }) => (
-                  <FormItem className="text-white">
-                    <FormControl>
-                      <Input
-                        {...field}
-                        placeholder={fieldPlaceholder}
-                        className=" bg-transparent text-white !text-form-text !border-b-[2px] !border-b-white w-full border-t-0 border-x-0 rounded-none"
-                      />
-                    </FormControl>
-                    {optional && <p className="text-white text-description">*Optional</p>}
-                    <FormMessage/>
-                  </FormItem>
-                )}
-              />
-        )}
-      </div>
+        ) : (
+                  <input
+                    value={isVip}
+                    placeholder={fieldPlaceholder}
+                    className="bg-transparent text-white !text-form-text !border-b-[2px] !border-b-white w-full border-t-0 border-x-0 rounded-none"
+                  />
+                  {optional && (
+                    <p className="text-white text-description">*Optional</p>
+                  )}
+            )}
+        </div>
     </section>
   );
 };
