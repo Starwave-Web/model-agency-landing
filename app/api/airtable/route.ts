c9577511471093eb36bc2c6f793731b680729755
udvardiendre <udvardiendre@gmail.com>
2025-04-16 21:43:21 +0200

    Csonti fixes 10: Caching issues related to Netlify Edge

--- app/api/airtable/route.ts
+++ app/api/airtable/route.ts
@@ -1,24 +1,40 @@
 export async function POST(request: Request) {
-    try {
-      const data = await request.json();
-      const records = [{ fields: data }];
-  
-      const response = await fetch("https://api.airtable.com/v0/appnCd1yfTghNwMMp/Elluxe%20Leads", {
-        method: 'POST',
+  try {
+    const data = await request.json();
+    const records = [{ fields: data }];
+
+    const response = await fetch(
+      "https://api.airtable.com/v0/appnCd1yfTghNwMMp/Elluxe%20Leads",
+      {
+        method: "POST",
         headers: {
           Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ records, typecast: true }),
-      });
-  
-      if (!response.ok) throw new Error(`Error posting to Airtable: ${response.statusText}`);
-  
-      return new Response(await response.text(), { status: response.status });
-    } catch (error) {
-      return new Response(JSON.stringify({ error: error instanceof Error ? error.message : error }), {
+      }
+    );
+
+    if (!response.ok)
+      throw new Error(`Error posting to Airtable: ${response.statusText}`);
+
+    return new Response(await response.text(), {
+      status: response.status,
+      headers: {
+        "Cache-Control": "no-store",
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: error instanceof Error ? error.message : error }),
+      {
         status: 500,
-      });
-    }
+        headers: {
+          "Cache-Control": "no-store",
+          "Content-Type": "application/json",
+        },
+      }
+    );
   }
-  
+}
